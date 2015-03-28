68fc6635046a745476dab1b8ac4657e99f9e8a41
Umar Maqsud <umar.maqsud@campus.tu-berlin.de>
2015-03-28 14:01:07 +0100

    fixed Controller

--- nerdle-core/src/main/java/de/textmining/nerdle/evaluation/Controller.java
+++ nerdle-core/src/main/java/de/textmining/nerdle/evaluation/Controller.java
@@ -22,9 +22,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import de.textmining.nerdle.database.DBFactProvider;
 import de.textmining.nerdle.database.DBSingleton;
 import de.textmining.nerdle.database.FactProvider;
-import de.textmining.nerdle.database.MVFactProvider;
 import de.textmining.nerdle.database.MVSingleton;
 import de.textmining.nerdle.question.answering.MatchFactQuestionAnswerer;
 import de.textmining.nerdle.question.answering.QuestionAnswerer;
@@ -94,19 +94,19 @@ public class Controller {
 
         switch (topic) {
         case SIMPSONS:
+            factProvider = new DBFactProvider(dbSingleton.getConnections().get("simpsons"));
             // factProvider = new
-            // DBFactProvider(dbSingleton.getConnections().get("simpsons"));
-            factProvider = new MVFactProvider(mvSingleton.getConnections().get("simpsons"));
+            // MVFactProvider(mvSingleton.getConnections().get("simpsons"));
             break;
         case STAR_TREK:
+            factProvider = new DBFactProvider(dbSingleton.getConnections().get("star-trek"));
             // factProvider = new
-            // DBFactProvider(dbSingleton.getConnections().get("star-trek"));
-            factProvider = new MVFactProvider(mvSingleton.getConnections().get("star-trek"));
+            // MVFactProvider(mvSingleton.getConnections().get("star-trek"));
             break;
         case STAR_WARS:
+            factProvider = new DBFactProvider(dbSingleton.getConnections().get("star-wars"));
             // factProvider = new
-            // DBFactProvider(dbSingleton.getConnections().get("star-wars"));
-            factProvider = new MVFactProvider(mvSingleton.getConnections().get("star-wars"));
+            // MVFactProvider(mvSingleton.getConnections().get("star-wars"));
             break;
         }
 
